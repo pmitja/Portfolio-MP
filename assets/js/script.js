17aaf7261ef061ce90f7cdd54668a9f6930456af
Mitja Pak <pmitja1@gmail.com>
2021-12-06 20:25:19 +0100

    repairing smooth scrolling behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+/////////////////////////////////////////////
+/// Smooth scrolling
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const el = document.querySelector(href);
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
   origin: "top",
